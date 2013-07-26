46279f25b0a553b1c3732ff4ad460cfbd905f4e9
Ryan Liang <ryanliang@gmail.com>
2013-07-25 23:21:32 -0400

    added comment and default value for new_modal_dialog method

--- pages/modal/child_page.rb
+++ pages/modal/child_page.rb
@@ -8,7 +8,19 @@ class ChildPage
   button(:open_modal_popup, id: "OpenPopupButton")
   button(:close, id: "MyModalPopup")
 
-  def new_modal_dialog(name, &block)
+  #
+  # Based on the modal_dialog from page-object gem.
+  # Override window.showModalDialog with window.open to
+  # handle a modal window.
+  #
+  # @example
+  #   @page.new_modal_dialog('_blank') do
+  #     @page.action_that_spawns_the_modal
+  #   end
+  #
+  # @param [String] name of window in which to load the URL. '_blank' will force a new window
+  # @param block a block that contains the call that will cause the modal dialog
+  def new_modal_dialog(name = 'modal', &block)
     script =
       %Q{
         window.showModalDialog = function(sURL, vArguments, sFeatures) {
