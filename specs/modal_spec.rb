86a57975b690641948a580be580226b3a866cd0b
Ryan Liang <ryanliang@gmail.com>
2013-07-25 21:52:52 -0400

    added original instructions

--- specs/modal_spec.rb
+++ specs/modal_spec.rb
@@ -1,6 +1,12 @@
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), "../pages/modal", "./"))
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), "./", "./"))
 
+# Write a program using Selenium Webdriver
+# Open up the "parent.htm" file in Firefox.
+# Then click the "Open Modal Popup" button.
+# In the pop-up, click "Open Modal Popup" again.
+# Close all windows by clicking the Close buttons in the Pop-Ups
+# Use any programming languages and libraries you need
 
 require 'selenium-webdriver'
 require 'parent_page'
